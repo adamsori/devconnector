14b1e65a2efdf54eed59aa8f13a6b744a3484525
Adam Sori <adam.sori@gmail.com>
2022-04-11 00:46:24 -0300

    Primeiro commit client

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// After
+import { createRoot } from 'react-dom/client';
+import App from './App';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
