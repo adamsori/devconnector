a490ce22978b9b3bf622d11832113648dd9a8407
Adam Sori <adam.sori@gmail.com>
2022-07-12 19:32:28 -0300

    Dashboard access rules, spinner added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
-    return (
-        <section className="container"><div>Dashboard</div></section>
-    )
+    return loading && profile === null ? <section className='container'><Spinner /></section> : <section className='container'>
+        <h1 className='large text-primary'>Dashboard</h1>
+        <p className='lead'>
+            <i className='fas fa-user'></i> Welcome {user && user.name}
+        </p>
+    </section>
 }
 
 Dashboard.propTypes = {
